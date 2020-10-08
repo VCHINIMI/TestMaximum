7322e354953dd7f5e722c609884d0490de01374a
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-08 19:19:47 +0530

    [Vinay]Refactored: Used one generic function instead of individual functions

--- TestMaximum.java
+++ TestMaximum.java
@@ -2,7 +2,7 @@ package com.vinay.TestMaximum;
 
 public class TestMaximum {
 //	Max Integer Function	
-	public static Integer maxInteger(Integer x, Integer y, Integer z) {
+	public static Integer max(Integer x, Integer y, Integer z) {
 		int maxInteger = x;
 		if (y.compareTo(maxInteger) > 0)
 			maxInteger = y;
@@ -12,7 +12,7 @@ public class TestMaximum {
 	}
 
 //Max Float Function	
-	public static Float maxFloat(Float x, Float y, Float z) {
+	public static Float max(Float x, Float y, Float z) {
 		float maxFloat = x;
 		if (y.compareTo(maxFloat) > 0)
 			maxFloat = y;
@@ -21,4 +21,23 @@ public class TestMaximum {
 		return maxFloat;
 	}
 
+//Max Length String Function
+	public static int max(String x, String y, String z) {
+		int maxStringLength = x.length();
+		if ((y.length()) - (maxStringLength) > 0)
+			maxStringLength = y.length();
+		if (z.length() - maxStringLength > 0)
+			maxStringLength = z.length();
+		return maxStringLength;
+	}
+
+//Generic Function 
+	public static <T extends Comparable<T>> T max(T a, T b, T c) {
+		T max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		return max;
+	}
 }
--- TestMaximumTest.java
+++ TestMaximumTest.java
@@ -13,6 +13,9 @@ class TestMaximumTest {
 	public static Float x = new Float(6);
 	public static Float y = new Float(7);
 	public static Float z = new Float(8);
+	public static String m = new String("Apple");
+	public static String n = new String("Peach");
+	public static String o = new String("Banana");
 
 	@BeforeClass
 	public static void intialize() {
@@ -22,38 +25,65 @@ class TestMaximumTest {
 //Test Cases for Maximum in Integers
 	@Test
 	public void testMaximumWhenMaximumIsAtFirstPosition() {
-		int test = testMax.maxInteger(a, b, c);
+		int test = testMax.max(a, b, c);
 		assertEquals(6, test);
 	}
 
 	@Test
 	public void testMaximumWhenMaximumIsAtSecondPosition() {
-		int test = testMax.maxInteger(b, a, c);
+		int test = testMax.max(b, a, c);
 		assertEquals(6, test);
 	}
 
 	@Test
 	public void testMaximumWhenMaximumIsAtThirdPosition() {
-		int test = testMax.maxInteger(b, c, a);
+		int test = testMax.max(b, c, a);
 		assertEquals(6, test);
 	}
 
 //Test Cases for Maximum in Float
 	@Test
 	public void testMaximumFloatWhenMaximumIsAtFirstPosition() {
-		float test = testMax.maxFloat(z, x, y);
+		float test = testMax.max(z, x, y);
 		assertEquals(8, test);
 	}
 
 	@Test
 	public void testMaximumFloatWhenMaximumIsAtSecondPosition() {
-		float test = testMax.maxFloat(y, z, x);
+		float test = testMax.max(y, z, x);
 		assertEquals(8, test);
 	}
 
 	@Test
 	public void testMaximumFloatWhenMaximumIsAtThirdPosition() {
-		float test = testMax.maxFloat(x, y, z);
+		float test = testMax.max(x, y, z);
 		assertEquals(8, test);
 	}
+
+//Test Cases for Maximum length of String
+	@Test
+	public void testMaximumStringWhenMaximumIsAtFirstPosition() {
+		int test = testMax.max(o, n, m);
+		assertEquals(o.length(), test);
+	}
+
+	@Test
+	public void testMaximumStringWhenMaximumIsAtSecondPosition() {
+		int test = testMax.max(n, o, m);
+		assertEquals(o.length(), test);
+	}
+
+	@Test
+	public void testMaximumStringWhenMaximumIsAtThirdPosition() {
+		int test = testMax.max(n, m, o);
+		assertEquals(o.length(), test);
+	}
+
+//Test Cases for Generics
+	@Test
+	public void GenericstestMaximumWhenMaximumIsAtFirstPosition() {
+		int test = testMax.max(a, b, c);
+		assertEquals(6, test);
+	}
+
 }
