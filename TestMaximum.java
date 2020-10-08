c66a2bc08ee5828404a5c450272c27a554ae2ef2
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-08 22:14:57 +0530

    [Vinay]Added: Print method to be called in max function

--- TestMaximum.java
+++ TestMaximum.java
@@ -51,26 +51,22 @@ public class TestMaximum<T extends Comparable<T>> {
 	public T maxGeneric() {
 		return maxGeneric(this.genericG, this.genericE, this.genericF);
 	}
-/*
-//Generic Function 
-	public static <T extends Comparable<T>> T maxGeneric(T a, T b, T c) {
-		T max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
+
+// Print Method
+	public static <T> void printGeneric(T generic) {
+		System.out.println(generic);
+	}
+
+//Max Function with Generics, more than 3 parameters	
+	public static <T extends Comparable<T>> T maxGeneric(T... elements) {
+		T max = elements[0];
+		for (T element : elements) {
+			if (element.compareTo(max) > 0) {
+				max = element;
+			}
+		}
+		printGeneric(max);
 		return max;
 	}
-*/	
-	 public static <T extends Comparable<T>> T maxGeneric(T... elements) {
-	    T max = elements[0];
-	    for (T element : elements) {
-	    	
-	    	if (element.compareTo(max) > 0) {
-		            max = element;
-		        }
-		    }
-		    return max;
-		}
 
 }
