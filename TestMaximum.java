e181715328c3bf77b74c4f3687a719ed1d3926be
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-08 18:27:43 +0530

    [Vinay]Added: Maximum Float Method with Test cases TC 2.1, 2.2, 2.3

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,14 +1,24 @@
 package com.vinay.TestMaximum;
 
 public class TestMaximum {
-	
 //	Max Integer Function	
-	public static Integer maxInteger(Integer x,Integer y, Integer z) {
+	public static Integer maxInteger(Integer x, Integer y, Integer z) {
 		int maxInteger = x;
-		if(y.compareTo(maxInteger)>0)
+		if (y.compareTo(maxInteger) > 0)
 			maxInteger = y;
-		if (z.compareTo(maxInteger)>0)
+		if (z.compareTo(maxInteger) > 0)
 			maxInteger = z;
-		return maxInteger;		
+		return maxInteger;
 	}
+
+//Max Float Function	
+	public static Float maxFloat(Float x, Float y, Float z) {
+		float maxFloat = x;
+		if (y.compareTo(maxFloat) > 0)
+			maxFloat = y;
+		if (z.compareTo(maxFloat) > 0)
+			maxFloat = z;
+		return maxFloat;
+	}
+
 }
--- TestMaximumTest.java
+++ TestMaximumTest.java
@@ -10,6 +10,9 @@ class TestMaximumTest {
 	public static Integer a = new Integer(6);
 	public static Integer b = new Integer(5);
 	public static Integer c = new Integer(4);
+	public static Float x = new Float(6);
+	public static Float y = new Float(7);
+	public static Float z = new Float(8);
 
 	@BeforeClass
 	public static void intialize() {
@@ -20,18 +23,37 @@ class TestMaximumTest {
 	@Test
 	public void testMaximumWhenMaximumIsAtFirstPosition() {
 		int test = testMax.maxInteger(a, b, c);
-		assertEquals(6, a);
+		assertEquals(6, test);
 	}
 
 	@Test
 	public void testMaximumWhenMaximumIsAtSecondPosition() {
 		int test = testMax.maxInteger(b, a, c);
-		assertEquals(6, a);
+		assertEquals(6, test);
 	}
 
 	@Test
 	public void testMaximumWhenMaximumIsAtThirdPosition() {
 		int test = testMax.maxInteger(b, c, a);
-		assertEquals(6, a);
+		assertEquals(6, test);
+	}
+
+//Test Cases for Maximum in Float
+	@Test
+	public void testMaximumFloatWhenMaximumIsAtFirstPosition() {
+		float test = testMax.maxFloat(z, x, y);
+		assertEquals(8, test);
+	}
+
+	@Test
+	public void testMaximumFloatWhenMaximumIsAtSecondPosition() {
+		float test = testMax.maxFloat(y, z, x);
+		assertEquals(8, test);
+	}
+
+	@Test
+	public void testMaximumFloatWhenMaximumIsAtThirdPosition() {
+		float test = testMax.maxFloat(x, y, z);
+		assertEquals(8, test);
 	}
 }
